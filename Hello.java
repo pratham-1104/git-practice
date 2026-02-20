3036ef50a5a2d34d0892f30f1658146ae4629e1b
Prathmesh Kangude <prathmeshkangude1108@gmail.com>
2026-02-20 15:40:56 +0530

    practise of adding in git and commit

---
+++ Hello.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("Welcome to Java programming.");
+        System.out.println("This is a simple Java program.");
+        System.out.println("Have a great day!");
+    }
+}
